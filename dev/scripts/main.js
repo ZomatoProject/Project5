76815ec6e6c87bc493bade4dcb4db3b8071cd7e0
Khim <khimsoy7@gmail.com>
2017-06-04 22:58:27 -0400

    added estela css

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -276,7 +276,7 @@ app.cuisineMatch = function(restaurantRes) {
 //append to the restaurantContainer in APP (.forEACH)
 app.displayFinalThree = function(finalThree) {
   app.finalThree.forEach(function(finalThree) {
-      $('#restaurantContainer').empty();
+      $('restaurantItem').remove();
       const restaurantItem = $('<li>').addClass('restaurantItem');
      
       const restName = $('<p class="restaurantName">').text(finalThree.name);
